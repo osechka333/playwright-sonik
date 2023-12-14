d7191c235feb6869991b52f3b589177053281a02
opokolenko <ospokolenko@gmail.com>
2023-12-14 11:48:50 +0200

    updatedFolderName

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ dotenv.config({
     path:  process.env.ENV ? `./.env.${process.env.ENV}` : '../.env'
 });
 
-console.log("Using env : ", process.env.ENV);
+console.log("Using env : ", dotenv.config.path);
 
 export const config = {
     baseURL: process.env.BASE_URL,
